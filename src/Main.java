51db5ec459ec0e755017813da960df29d675fff4
unfaiyted <unfaiyted@gmail.com>
2018-03-29 11:06:32 -0500

    short hand operators added

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
+//int class = 2; /bad
 
-        int favNumber = 1;
+        float favNumber = 3.14F;
 
         System.out.println(favNumber);
 
@@ -12,6 +13,22 @@ public class Main {
 
         System.out.println(myString);
 
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+         x = 4;
+        x+= 5;
+
+        x = 3;
+        int y = 4;
+        y*=x;
+
+
+        x = 10;
+        y = 2;
+        x /= y;
+        y -= x;
 
 
 
