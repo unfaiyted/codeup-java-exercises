2b95cee889a1cbc8d6b04a4cb8bba2169040097c
unfaiyted <unfaiyted@gmail.com>
2018-03-29 10:52:54 -0500

    initial var declaration.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        int favNumber = 1;
+
+        System.out.println(favNumber);
+
+        String myString = "This is my string. Save me";
+
+        System.out.println(myString);
+
+
+
+
+    }
+}
