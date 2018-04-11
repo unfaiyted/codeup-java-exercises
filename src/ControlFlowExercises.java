fcc7e8d9cccdc35cebd5b8b6801907b72f30a309
unfaiyted <unfaiyted@gmail.com>
2018-04-11 11:46:23 -0500

    do while, evens to 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -13,15 +13,37 @@ public class ControlFlowExercises {
 //
 //        5 6 7 8 9 10 11 12 13 14 15
 
-        int i = 5;
+//        int i = 5;
+//
+//        while (i <= 15) {
+//
+//            System.out.print(i + " ");
+//
+//            i++;
+//
+//        }
 
-        while (i <= 15) {
+//        Do While
+//
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//
+//        2
+//        4
+//        16
+//        256
+//        65536
 
-            System.out.print(i + " ");
+        int number = 2;
 
-            i++;
+        do {
+
+            System.out.println(number);
+            number = number+2;
 
         }
+        while (number <= 100);
 
 
 
