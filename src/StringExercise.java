a7f4ccf5d8589c6922f9bbe7616ea649c4eb5f9b
unfaiyted <unfaiyted@gmail.com>
2018-04-11 13:39:35 -0500

    adding escape sequences to strings

---
+++ src/StringExercise.java
@@ -0,0 +1,26 @@
+public class StringExercise {
+
+
+    public static void main(String[] args) {
+
+            String message =
+
+                    "We don't need no education\n" +
+                            "We don't need no thought control\n" +
+                            "\n" +
+                            "Check \"this\" out!, \"s inside of \"s!\n" +
+                            "\n" +
+                            "In windows, the main drive is usually C:\\\n" +
+                            "\n" +
+                            "I can do backslashes \\, double backslashes \\\\,\n" +
+                            "and the amazing triple backslash \\\\\\!" +
+                            "";
+
+            System.out.print(message);
+
+
+    }
+
+
+
+}
