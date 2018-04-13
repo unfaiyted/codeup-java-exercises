6007ae9b3b5eda6480851004d76a61e107ec7382
unfaiyted <unfaiyted@gmail.com>
2018-04-13 10:11:27 -0500

    hashmap added

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,20 +1,47 @@
 package grades;
 
+import java.util.HashMap;
+
 public class GradesApplication {
 
     public static void main(String[] args) {
 
 
-        Student tim = new Student("Timmo");
+        HashMap<String, Student> students = new HashMap<>();
 
-        tim.addGrade(100);
-        tim.addGrade(100);
+        students.put("Xikage",new Student("TimBo"));
+        students.put("Xolaeth", new Student("Hesstopher"));
+        students.put("Akira", new Student("Bosnanai"));
+        students.put("Faiyt", new Student("Dan"));
 
-       System.out.println("Avg: " + tim.getGradeAverage());
+        students.get("Xikage").addGrade(55);
+        students.get("Xikage").addGrade(10);
+        students.get("Xikage").addGrade(100);
 
-        tim.addGrade(50);
+        students.get("Xolaeth").addGrade(55);
+        students.get("Xolaeth").addGrade(10);
+        students.get("Xolaeth").addGrade(100);
 
-        System.out.println("Avg: " + tim.getGradeAverage());
+        students.get("Akira").addGrade(55);
+        students.get("Akira").addGrade(10);
+        students.get("Akira").addGrade(95);
+
+        students.get("Faiyt").addGrade(100);
+        students.get("Faiyt").addGrade(99);
+        students.get("Faiyt").addGrade(100);
+        students.get("Faiyt").addGrade(100);
+
+
+//        Student tim = new Student("Timmo");
+//
+//        tim.addGrade(100);
+//        tim.addGrade(100);
+//
+//       System.out.println("Avg: " + tim.getGradeAverage());
+//
+//        tim.addGrade(50);
+//
+//        System.out.println("Avg: " + tim.getGradeAverage());
 
 
     }
